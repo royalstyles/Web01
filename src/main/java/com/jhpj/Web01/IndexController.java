95045b1b90f02512ff1478bd088f9f5e98a1d3a0
Jungho Kim <royalstyles@naver.com>
2026-03-16 16:16:02 +0900

    로그아웃 기능 추가 index.html 접근 안되도록 수정

--- src/main/java/com/jhpj/Web01/IndexController.java
+++ src/main/java/com/jhpj/Web01/IndexController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-    @GetMapping("/")
-    public String index() {
-        return "Hello World!";
-    }
+//    @GetMapping("/")
+//    public String index() {
+//        return "Hello World!";
+//    }
 }
